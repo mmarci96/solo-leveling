9100ac6cacf005e45403d717d2cfab7645940252
Marci Sarodi <57949298+mmarci96@users.noreply.github.com>
2024-05-14 14:19:50 +0200

    header function to navigate added

--- weebhive-frontend/src/components/header/Header.jsx
+++ weebhive-frontend/src/components/header/Header.jsx
@@ -1,13 +1,20 @@
 import './Header.css'
+import NavButtons from './NavButtons'
+import { useState } from 'react'
 
 const Header = () => {
+  const [showOptions, setShowOptions] = useState(false);
+  const clickOptions = () => {
+    showOptions ? setShowOptions(false) : setShowOptions(true)
+  }
 
   return (
     <div id='header'>
       <div id='logo-container'><img id='icon' src='../icon.png'></img><h1 id='logo-title'>WeebHive</h1></div>
 
       <div id='nav-bar-container'>
-        <button>Options</button>
+        <button id='options' onClick={()=> clickOptions()}>Options</button>
+        {showOptions ? <NavButtons/> : <></>}
       </div>
 
     </div>
--- weebhive-frontend/src/components/header/NavButtons.jsx
+++ weebhive-frontend/src/components/header/NavButtons.jsx
@@ -1,13 +1,16 @@
+
 const NavButtons = props => {
   const handleAnimeButton = e => {
     console.log(e);
-    window.location = '/browse'
+    window.location = '/browse';
   }
   return(
     <div id='nav-button-container'>
-      <button onClick={()=>handleAnimeButton}>Anime</button>
+      <button onClick={()=>handleAnimeButton()}>Anime</button>
       <button>Forum</button>
       <button>My profile</button>
     </div>
   )
 }
+
+export default NavButtons;
